bd604f6610e62f6fb8f139453784edd59dce9bbc
Alan Almaguer <alalmag2@gmail.com>
2025-03-22 02:21:21 -0600

    Only fetch cart items if the token exists (user is logged in)

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,7 +1,8 @@
-'use client'
+'use client';
 
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import { getCartItemsNumber } from '@/app/utils/api';
+import Cookies from 'js-cookie';
 
 const CartContext = createContext<any>(null);
 
@@ -11,7 +12,8 @@ export const CartProvider: React.FC<{ children: React.ReactNode }> = ({ children
   const [cartItemsCount, setCartItemsCount] = useState<number>(0);
 
   useEffect(() => {
-    // Fetch the cart item count whenever the user logs in
+    // Only fetch cart items if the token exists (user is logged in)
+    if (!Cookies.get('token')) return;
     const fetchCartItems = async () => {
       try {
         const itemsCount = await getCartItemsNumber();
@@ -25,6 +27,8 @@ export const CartProvider: React.FC<{ children: React.ReactNode }> = ({ children
   }, []);
 
   const updateCartCount = async () => {
+    // Only update the cart count if the token exists
+    if (!Cookies.get('token')) return;
     try {
       const itemsCount = await getCartItemsNumber();
       setCartItemsCount(itemsCount);
